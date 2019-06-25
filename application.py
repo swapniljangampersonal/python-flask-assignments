49c6a49c2b0fce68202ab3ca62433945cbd61ff9
Swapnil Jangam <neel@Swapnils-MacBook-Air.local>
2019-06-25 17:20:01 -0500

    latest

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from sklearn.metrics import silhouette_score
 import pandas as pd
 import numpy as np
@@ -9,14 +9,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import datetime
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics import pairwise_distances_argmin_min
+from flask.ext.session import Session
 
 minnow = "static/minnow.csv"
 trained_minnow = pd.read_csv(minnow)
 trained_minnow.fillna(trained_minnow.mean(), inplace=True)
+sess = Session()
 
 labelEncoder = LabelEncoder()
 labelEncoder.fit(trained_minnow['Survived'])
 trained_minnow['Survived'] = labelEncoder.transform(trained_minnow['Survived'])
+global_kmeans = ''
+global_x = ''
+global_mydict = ''
 
 # print(trained_minnow.info())
 
@@ -35,8 +41,10 @@ def kmeans():
     start_time = datetime.datetime.now()
     kmeans = KMeans(n_clusters=k_input)
     kmeans = kmeans.fit(trained_minnow[[x_column, y_column]])
+    session['kmeans'] = kmeans
     centroids = kmeans.cluster_centers_
     clusters = kmeans.fit_predict(trained_minnow[[x_column, y_column]])
+    session['X'] = trained_minnow[[x_column, y_column]]
     distances = []
     max_dist = []
     for i, (cx, cy) in enumerate(centroids):
@@ -47,6 +55,7 @@ def kmeans():
     print(max_dist)
     end_time = datetime.datetime.now()
     mydict = {i: np.where(kmeans.labels_ == i)[0] for i in range(k_input)}
+    session['mydict'] = mydict
     total_time = end_time - start_time
     return render_template('index.html', centroids=centroids, result=mydict, max_indices=max_dist)
 
@@ -55,6 +64,21 @@ def k_mean_distance(data, cx, cy, i_centroid, cluster_labels):
     for (x, y) in data[cluster_labels == i_centroid].values:
         distances.append(np.sqrt((x-cx)**2+(y-cy)**2))
     return distances
+
+@application.route('/kmeansglobal', methods=['GET'])
+def globalkmeans():
+    x_co = float(request.args['x_co'])
+    y_co = float(request.args['y_co'])
+    centroids = session.get('kmeans').cluster_centers_
+    closest, _ = pairwise_distances_argmin_min(centroids, session.get('X'))
+    cluster_map = pd.DataFrame()
+    cluster_map['data_index'] = trained_minnow.index.values
+    cluster_map['cluster'] = session.get('kmeans').labels_
+    # print(cluster_map[cluster_map.cluster == np.where(centroids == [x_co, y_co])[0]-1])
+    # print(closest[np.where(centroids == [x_co, y_co])[0]-1])
+    mylist = np.where(centroids == [x_co, y_co])
+    print(mylist[0][0])
+    return render_template("newindex.html", result=trained_minnow['Fname'][mylist[0][0]])
 
 @application.route('/myrest', methods=['GET'])
 def myrest():
@@ -76,5 +100,9 @@ def myrest():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
+    application.secret_key = 'super secret key'
+    application.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(application)
     application.debug = True
     application.run()
